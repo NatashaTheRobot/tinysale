fc17dee8e9217ca34298999522c54054786decb3
NatashaMurashev <nmurashev@gmail.com>
2013-03-02 12:04:00 -0800

    refactoring

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,17 +5,7 @@ class CommentsController < ApplicationController
     @new_comment = Comment.build_from( params, user_id )
     if !@new_comment.spam? and @new_comment.save!
       cookies[:reviewer] = @new_comment.lead.token unless user_id.present?
-      #@comment_output = { title: @new_comment.title,
-      #                    avatar: view_context.author_image(@new_comment),
-      #                    author_name: view_context.author_name(@new_comment),
-      #                    rating: @new_comment.rating,
-      #                    body: @new_comment.body,
-      #                    date: @new_comment.created_at.strftime("%b %d, %Y")}
-      if params[:partial]
-        render :partial => "comment" , locals: { comment: @new_comment }
-      else
-        respond_with @comment_output, location: @new_comment, status: :ok
-      end
+      render :partial => "show" , locals: { comment: @new_comment }
     else
       # include what do when comment is spam first
       respond_with @new_comment.errors, location: @new_comment, status: :unprocessable_entity
