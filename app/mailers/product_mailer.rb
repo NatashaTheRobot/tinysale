4a618caff11a23329056e79b19bd822aa5b007dd
NatashaMurashev <nmurashev@gmail.com>
2013-03-10 10:16:39 -0700

    a product now goes out to the seller once a product is purchased

--- app/mailers/product_mailer.rb
+++ app/mailers/product_mailer.rb
@@ -1,11 +1,19 @@
 class ProductMailer < ActionMailer::Base
   default :from => 'natasha@tinysale.com'
 
-  def product_email(transaction)
+  def product_purchase_email(transaction)
     product = transaction.product
-    mail to: transaction.email, subject: "You just bought #{product.title}",
+    mail to: transaction.email,
+         subject: "You just bought #{product.title}",
          body: "Thank you for purchasing #{product.title} from #{product.user.username} for #{product.price_in_dollars}.\n
                 You can <a href='#{product.attachments.first.item.expiring_url(60)}' target='_blank'> download your copy here.</a> (Keep in mind that this link expires 60 seconds after you open it) \n
                 Enjoy!".html_safe
+  end
+
+  def product_sold_email(transaction)
+    product = transaction.product
+    mail to: product.user.email,
+         subject: "Cha-Ching! You just sold #{product.title}",
+         body: "Congratulations! You just sold #{product.title} to #{transaction.email}."
   end
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,10 +2,11 @@ class Transaction < ActiveRecord::Base
   belongs_to :product
   attr_accessible :email, :product
 
-  after_save :send_product
+  after_save :send_product_emails
 
-  def send_product
-    ProductMailer.product_email(self).deliver
+  def send_product_emails
+    ProductMailer.product_purchase_email(self).deliver
+    ProductMailer.product_sold_email(self).deliver
   end
 
 end
